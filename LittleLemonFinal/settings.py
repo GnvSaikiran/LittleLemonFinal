00d6ec3ea9280b45220d4edf9ed07a7bd39b5f20
gnv <gajjarapusai1@gmail.com>
2023-12-10 19:49:30 +0530

    Update allowed hosts

--- LittleLemonFinal/settings.py
+++ LittleLemonFinal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)fqhsyl5x_+k73e#om92vzykd562xcar-h+bg#iqm*rvx_xit^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['littlelemonre.up.railway.app']
 
 
 # Application definition
